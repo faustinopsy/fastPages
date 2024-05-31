b81c965dee2c62c170609969abb69571173c831d
faustinopsy <rodrigohipnose@gmail.com>
2024-05-31 01:51:10 -0300

    melhorias

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
 <head>
     <title>Gerenciador de Páginas</title>
-    <link href="public/css/materialize.min.css" rel="stylesheet">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link href="public/css/materialize.min.css" rel="stylesheet">
+                <link href="public/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <style>
         .step {
             display: none;
@@ -13,6 +16,12 @@
     </style>
 </head>
 <body>
+<nav class="teal lighten-1" role="navigation">
+                  <div class="nav-wrapper container">
+                  <a id="logo-container" href="#" class="brand-logo"><i class="material-icons">all_inclusive</i>FastPages</a>
+                  <ul class="right hide-on-med-and-down">
+                  </ul>
+                </div></nav>
     <div class="container">
         <h3>Gerenciador de Páginas</h3>
         <form id="multi-step-form" action="gerar_paginas.php" method="post" enctype="multipart/form-data">
