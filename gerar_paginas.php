fbcdf246d618a0d9c1c820cf08a2b189f23b18c8
faustinopsy <rodrigohipnose@gmail.com>
2024-05-31 02:09:18 -0300

    centralizando o menu e footer

--- gerar_paginas.php
+++ gerar_paginas.php
@@ -3,46 +3,93 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $pagina = $_POST['pagina'];
     $uploads_dir = 'uploads/';
     $acesso = 0;
-   
+
     if (!is_dir($uploads_dir)) {
         mkdir($uploads_dir, 0777, true);
     }
 
-    function gerar_menu($pagina) {
+    function gerar_menu_script($pagina) {
         $numero = 0;
-        $menu = '<nav class="light-blue lighten-1" role="navigation">
-                  <div class="nav-wrapper container">
-                  <a id="logo-container" href="#" class="brand-logo">Logo</a>
-                  <ul class="right hide-on-med-and-down">';
+        $menu_script = 'document.addEventListener("DOMContentLoaded", function() {
+            let menuHtml = `
+                <nav class="light-blue lighten-1" role="navigation">
+                    <div class="nav-wrapper container">
+                        <a id="logo-container" href="#" class="brand-logo">Logo</a>
+                        <ul class="right hide-on-med-and-down">';
+
         foreach ($pagina as $pagina_nome => $conteudo_pagina) {
-          if (is_int($pagina_nome)) {
-            continue;
+            if (is_int($pagina_nome)) {
+                continue;
+            }
+            $menu_script .= '<li><a href="' . htmlspecialchars($pagina_nome) . '.html">' . htmlspecialchars($pagina_nome) . '</a></li>';
         }
-            $menu .= '<li><a href="' . htmlspecialchars($pagina_nome) . '.html">' . htmlspecialchars($pagina_nome) . '</a></li>';
-        }
-        $menu .= '</ul><ul id="nav-mobile" class="sidenav">';
+
+        $menu_script .= '</ul><ul id="nav-mobile" class="sidenav">';
+
         foreach ($pagina as $pagina_nome => $conteudo_pagina) {
-          if (is_int($pagina_nome)) {
-            continue;
+            if (is_int($pagina_nome)) {
+                continue;
+            }
+            $numero++;
+            $menu_script .= '<li><a href="' . htmlspecialchars($pagina_nome) . '.html"><i class="material-icons">filter_' . $numero . '</i>' . htmlspecialchars($pagina_nome) . '</a></li>';
         }
-            $numero=$numero+1;
-            $menu .= '<li><a href="' . htmlspecialchars($pagina_nome) . '.html">
-            <i class="material-icons">filter_' . $numero . '</i>' . htmlspecialchars($pagina_nome) . '</a></li>';
-        }
-        $menu .= '</ul>
-                  <a href="#" data-target="nav-mobile" class="sidenav-trigger">
-                  <i class="material-icons">menu</i>
-                  </a>
-                  </div>
-                  </nav>';
-                  
-        return $menu;
+
+        $menu_script .= '</ul>
+                    <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                </div>
+            </nav>`;
+
+        document.getElementById("menu-container").innerHTML = menuHtml;
+        M.Sidenav.init(document.querySelectorAll(".sidenav"));
+
+        let footerHtml = `
+            <footer class="page-footer orange">
+                <div class="container">
+                    <div class="row">
+                        <div class="col l6 s12">
+                            <h5 class="white-text">Company Bio</h5>
+                            <p class="grey-text text-lighten-4">We are a team of college students working on this project like its our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+                        </div>
+                        <div class="col l3 s12">
+                            <h5 class="white-text">Settings</h5>
+                            <ul>
+                                <li><a class="white-text" href="#!">Link 1</a></li>
+                                <li><a class="white-text" href="#!">Link 2</a></li>
+                                <li><a class="white-text" href="#!">Link 3</a></li>
+                                <li><a class="white-text" href="#!">Link 4</a></li>
+                            </ul>
+                        </div>
+                        <div class="col l3 s12">
+                            <h5 class="white-text">Connect</h5>
+                            <ul>
+                                <li><a class="white-text" href="#!">Link 1</a></li>
+                                <li><a class="white-text" href="#!">Link 2</a></li>
+                                <li><a class="white-text" href="#!">Link 3</a></li>
+                                <li><a class="white-text" href="#!">Link 4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <div class="container">
+                        Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+                    </div>
+                </div>
+            </footer>`;
+
+        document.getElementById("footer-container").innerHTML = footerHtml;
+        });
+        ';
+
+        return $menu_script;
     }
 
-    $menu = gerar_menu($pagina);
+    $menu_script = gerar_menu_script($pagina);
+    file_put_contents('public/js/menuAndFooter.js', $menu_script);
+
     foreach ($pagina as $pagina_nome => $conteudo_pagina) {
         if (is_int($pagina_nome)) {
-          continue;
+            continue;
         }
         if ($acesso === 0) {
             $acesso = $pagina_nome;
@@ -57,7 +104,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
             </head>
         <body>
-            ' . $menu . '
+            <div id="menu-container"></div>
             <div class="container">';
         if (isset($conteudo_pagina['sections'])) {
             $html .= '<div class="row"><br>';
@@ -70,7 +117,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     $fileTmpPath = $_FILES['pagina']['tmp_name'][$pagina_nome]['sections'][$sectionIndex]['content'];
                     $fileName = basename($_FILES['pagina']['name'][$pagina_nome]['sections'][$sectionIndex]['content']);
                     $filePath = $uploads_dir . $fileName;
-                    if (move_uploaded_file($fileTmpPath, 'public/'.$filePath)) {
+                    if (move_uploaded_file($fileTmpPath, 'public/' . $filePath)) {
                         $sectionContent = '<img src="' . $filePath . '" alt="' . htmlspecialchars($pagina_nome) . '" style="width: 150px;height: 150px;">';
                     } else {
                         $sectionContent = '<p>Erro ao fazer upload da imagem.</p>';
@@ -110,40 +157,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <script src="js/jquery-2.1.1.min.js"></script>
         <script src="js/materialize.js"></script>
         <script src="js/init.js"></script>
+        <script src="js/menuAndFooter.js"></script>
+        <div id="footer-container"></div>
         </body>
-        <footer class="page-footer orange">
-        <div class="container">
-          <div class="row">
-            <div class="col l6 s12">
-              <h5 class="white-text">Company Bio</h5>
-              <p class="grey-text text-lighten-4">We are a team of college students working on this project like its our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-            </div>
-            <div class="col l3 s12">
-              <h5 class="white-text">Settings</h5>
-              <ul>
-                <li><a class="white-text" href="#!">Link 1</a></li>
-                <li><a class="white-text" href="#!">Link 2</a></li>
-                <li><a class="white-text" href="#!">Link 3</a></li>
-                <li><a class="white-text" href="#!">Link 4</a></li>
-              </ul>
-            </div>
-            <div class="col l3 s12">
-              <h5 class="white-text">Connect</h5>
-              <ul>
-                <li><a class="white-text" href="#!">Link 1</a></li>
-                <li><a class="white-text" href="#!">Link 2</a></li>
-                <li><a class="white-text" href="#!">Link 3</a></li>
-                <li><a class="white-text" href="#!">Link 4</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="footer-copyright">
-          <div class="container">
-          Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
-          </div>
-        </div>
-      </footer>
         </html>';
         file_put_contents('public/' . $pagina_nome . '.html', $html);
     }
